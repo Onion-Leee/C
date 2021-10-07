251e89318aca9d8da8a96886b320245e410805b5
Onion-Leee <1327968619@qq.com>
2021-10-07 13:07:16 +0800

    add malloc or free

--- C_Advanced_Review/10_6_malloc_free/10_6_malloc_free/malloc_free.c
+++ C_Advanced_Review/10_6_malloc_free/10_6_malloc_free/malloc_free.c
@@ -1,6 +1,18 @@
 
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+
 
 //栈区：局部变量+函数形参
 //堆区：动态内存开辟（malloc/calloc/free/realloc)
 //静态区（数据段）：全局变量+静态变量
+
+
+//需要注意的是malloc函数的返回值一定要做检查
+int main()
+{
+	int* p = (int*)malloc(10 * sizeof(int));
+
+	return 0;
+}
