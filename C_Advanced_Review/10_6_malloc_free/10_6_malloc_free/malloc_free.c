f7ae6750989315c1772fac57542752771def474e
Onion-Leee <1327968619@qq.com>
2021-10-07 19:17:03 +0800

    add malloc or free

--- C_Advanced_Review/10_6_malloc_free/10_6_malloc_free/malloc_free.c
+++ C_Advanced_Review/10_6_malloc_free/10_6_malloc_free/malloc_free.c
@@ -13,6 +13,20 @@
 int main()
 {
 	int* p = (int*)malloc(10 * sizeof(int));
-
+	if (p == NULL)
+	{
+		perror("main");
+		return 0;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		*(p + i) = i;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d ", p[i]);
+	}
+	free(p);
+	p = NULL;
 	return 0;
 }
