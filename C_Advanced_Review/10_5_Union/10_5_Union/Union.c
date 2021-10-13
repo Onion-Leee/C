cff3c78d9c64a40e02d93982835aba0f9567da77
Onion-Leee <1327968619@qq.com>
2021-10-13 21:28:03 +0800

    add Union

--- C_Advanced_Review/10_5_Union/10_5_Union/Union.c
+++ C_Advanced_Review/10_5_Union/10_5_Union/Union.c
@@ -1,16 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-
-union Un
-{
-	char c;
-	int i;
-};
+//
+//union Un
+//{
+//	char c;
+//	int i;
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));
+//	return 0;
+//}
 
 int main()
 {
-	union Un u;
-	printf("%d\n", sizeof(u));
+	int i = 0;
+	for (i++; i++ < 4;)
+	{
+		printf("%d", i);
+	}
+	printf("\n%d", i);
 	return 0;
 }
+
